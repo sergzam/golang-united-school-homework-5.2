93b1583d91e53b75c3d646728cff9315960fa898
Sergii Zamashnykh <Sergii.Zamashnykh@refinitiv.com>
2022-05-24 16:24:35 +0300

    finished

--- cache.go
+++ cache.go
@@ -3,21 +3,47 @@ package cache
 import "time"
 
 type Cache struct {
+	values map[string]cachedValue
 }
 
+type cachedValue struct {
+	value    string
+	deadline time.Time
+}
+
+//goland:noinspection GoUnusedExportedFunction
 func NewCache() Cache {
-	return Cache{}
+	return Cache{values: make(map[string]cachedValue)}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c Cache) Get(key string) (string, bool) {
+	item, ok := c.values[key]
+	if !ok {
+		return "", false
+	}
 
+	if item.deadline.IsZero() || item.deadline.After(time.Now()) {
+		return item.value, true
+	}
+
+	delete(c.values, key)
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	item := cachedValue{value: value}
+	c.values[key] = item
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	keys := make([]string, 0, len(c.values))
+	for k := range c.values {
+		keys = append(keys, k)
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
+	item := cachedValue{value: value, deadline: deadline}
+	c.values[key] = item
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sergzam/golang-united-school-homework-5.2
+
+go 1.18
